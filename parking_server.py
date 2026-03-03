5c1ec4773a290ab6ca315a85ca7735edfada08d6
Caleb Lee <81351320+LeeSung-min@users.noreply.github.com>
2026-03-03 08:57:19 -0800

    Update parking_server.py

--- parking_server.py
+++ parking_server.py
@@ -1,1 +1,21 @@
+import socket
+import json
+import threading
+import parking_data
+import queue
 
+def worker():
+    while True:
+        conn, addr = 
+
+
+def main():
+    #load config 
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    port = config["server_port"]
+    parking = parking_data.Parking(config["lots"], config["ttl"])
+
+    # use thread pool to handle large amount of clients instead of thread per connection
+    pool
